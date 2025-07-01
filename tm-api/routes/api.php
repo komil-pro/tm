997f32b63144a32f377835d3914dc87290d95ea8
komil-pro <komil.tj@yandex.ru>
2025-07-01 11:31:43 +0500

    clean extra comments in routes

--- tm-api/routes/api.php
+++ tm-api/routes/api.php
@@ -12,9 +12,8 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',[AuthController::class,'login']);
-//Route::get('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('tasks', TaskController::class);
 });
-//Route::apiResource('tasks', TaskController::class);
